e0c4c09986c2f8c0c2c6be0acefa379ee1ceb38a
yuki-h29 <yuki.hirano29@gmail.com>
2023-05-31 00:20:00 +0900

    testCommit2

--- testStackView/ViewController.swift
+++ testStackView/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         var test = "1234"
+        print(test)
     }
 
 
